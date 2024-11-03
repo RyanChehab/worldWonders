38b8e7c4a682755d9422913eeb7db6d06dc971e5
unknown <ryanchehab20@gmail.com>
2024-11-03 22:08:03 +0200

    retrieved the wonder's names

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let container = document.getElementById('container') 
+
 fetch('https://www.world-wonders-api.org/v0/wonders')
 .then(response=> response.json())
-.then(data =>{
-    console.log(data)
-})
+.then(data=>
+    data.forEach(wonder =>{
+    const container = document.getElementById('container')
+        const wonderdiv = document.createElement('div')
+        wonderdiv.classList.add('wonder-item')
+        // get the name 
+        const name = document.createElement('h3')
+        name.textContent = wonder.name;
+
+        // add the name to each div
+        wonderdiv.appendChild(name)
+
+        
+
+        // append everything to the parent
+        container.appendChild(wonderdiv)
+    }) )
 .catch(error =>{
     console.error('Error',error)
 })

db3ee010b890fd8962b7df0058c3f72d3e22b184
unknown <ryanchehab20@gmail.com>
2024-11-03 22:37:05 +0200

    added wonder's details dynamically

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ fetch('https://www.world-wonders-api.org/v0/wonders')
         const wonderdiv = document.createElement('div')
         wonderdiv.classList.add('wonder-item')
         // get the name 
-        const name = document.createElement('h3')
+        const name = document.createElement('h2')
         name.textContent = wonder.name;
+        // get summary
+        const summary = document.createElement('p')
+        summary.textContent = wonder.summary;
+        // get location
+        const location = document.createElement('p')
+        location.classList.add('loc')
+        location.textContent = wonder.location
+        // get build year
+        const year = document.createElement('h3')
+        year.classList.add('year')
+        year.textContent = wonder.build_year
+        // get period
+        const time_period = document.createElement('h4')
+        time_period.innerText = wonder.time_period
 
         // add the name to each div
         wonderdiv.appendChild(name)
-
-        
+        wonderdiv.appendChild(summary)
+        wonderdiv.appendChild(location)
+        wonderdiv.appendChild(year)
+        wonderdiv.appendChild(time_period)
+        // wonderdiv.appendChild(year)
 
         // append everything to the parent
         container.appendChild(wonderdiv)

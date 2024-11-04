255f9d4620db9dcb1f5137cfb3a6882f1ddf6c8a
unknown <ryanchehab20@gmail.com>
2024-11-04 02:01:57 +0200

    added hero page

--- index.js
+++ index.js
@@ -19,21 +19,42 @@ fetch('https://www.world-wonders-api.org/v0/wonders')
         // get build year
         const year = document.createElement('h3')
         year.classList.add('year')
-        year.textContent = wonder.build_year
+        year.textContent = `Year: ${wonder.build_year}`
         // get period
         const time_period = document.createElement('h4')
-        time_period.innerText = wonder.time_period
+        time_period.innerText = `Time Period:  ${wonder.time_period}`
+        // get img
+        const image = document.createElement('img')
+            image.src = wonder.links.images[0]
+            image.alt = wonder.name
+            image.style.height = "250px"
 
+        // get categories
+        const categories = document.createElement('h3')
+        categories.innerText = wonder.categories
+        
+        const learn = document.createElement('p')
+        learn.style.color = 'blue'
+        learn.innerText = 'Explore more details...'
+        
         // add the name to each div
         wonderdiv.appendChild(name)
-        wonderdiv.appendChild(summary)
-        wonderdiv.appendChild(location)
-        wonderdiv.appendChild(year)
-        wonderdiv.appendChild(time_period)
-        // wonderdiv.appendChild(year)
-
+        wonderdiv.appendChild(image)
+        wonderdiv.appendChild(learn)
+        
         // append everything to the parent
         container.appendChild(wonderdiv)
+
+
+        wonderdiv.addEventListener('click', ()=>{
+            window.location.href = `details.html?name=${encodeURIComponent(wonder.name)}`;
+        })
+
+        // wonderdiv.appendChild(summary)
+        // wonderdiv.appendChild(location)
+        // wonderdiv.appendChild(year)
+        // wonderdiv.appendChild(categories)
+        // wonderdiv.appendChild(time_period)
     }) )
 .catch(error =>{
     console.error('Error',error)
